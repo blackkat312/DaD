8b6b0d9d633ba5f87f0174c1730b230602e54700
Moss <134397512+blackkat312@users.noreply.github.com>
2024-06-20 22:25:12 -0400

    Update ProfileScreen.py

--- scripts/screens/ProfileScreen.py
+++ scripts/screens/ProfileScreen.py
@@ -2435,6 +2435,9 @@ class ProfileScreen(Screens):
             "curved spine": "scoliosis",
             "jumbled mind": "dyslexia",
             "counting fog": "dyscalculia",
+            "spirited heart": "hyperempathy",
+            "puzzled heart": "low empathy",
+            "parrot chatter": "echolalia",
 
             "sunblindness": "light sensitivity",
 
