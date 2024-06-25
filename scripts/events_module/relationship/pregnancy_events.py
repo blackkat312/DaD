4d1bb11396fdceb8b71000eb36a7cf1a8b7da7f5
THybrid21 <124342283+THybrid21@users.noreply.github.com>
2024-06-25 19:39:05 +1000

    FIXED

--- scripts/events_module/relationship/pregnancy_events.py
+++ scripts/events_module/relationship/pregnancy_events.py
@@ -251,9 +251,6 @@ class Pregnancy_Events():
         if not clan.clan_settings["same sex birth"] and cat.gender == "male":
             return
             
-        # here's where we check for infertility, just in case ir slipped trough
-        if (cat and "infertile" in cat.permanent_condition) and not (other_cat and "infertile" in other_cat.permanent_condition):
-            return
         
         clan.pregnancy_data[cat.ID] = {
             "second_parent": str(other_cat.ID) if other_cat else None,
@@ -285,6 +282,10 @@ class Pregnancy_Events():
         
         # additional save for no kit setting
         if (cat and cat.no_kits) or (other_cat and other_cat.no_kits):
+            return
+
+        # here's where we check for infertility, just in case ir slipped trough
+        if (cat and "infertile" in cat.permanent_condition) and not (other_cat and "infertile" in other_cat.permanent_condition):
             return
 
         if clan.clan_settings['same sex birth']:
