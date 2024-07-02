1887a04b6144e51921d2cb002b040bc672b4d1d4
Moss <134397512+blackkat312@users.noreply.github.com>
2024-07-01 20:21:49 -0400

    Update load_cat.py

--- scripts/game_structure/load_cat.py
+++ scripts/game_structure/load_cat.py
@@ -162,6 +162,8 @@ def json_load():
             new_cat.no_mates = cat["no_mates"] if "no_mates" in cat else False
             new_cat.no_retire = cat["no_retire"] if "no_retire" in cat else False
             new_cat.neutered = cat["neutered"] if "neutered" in cat else False
+            new_cat.already_gave_neutered_message = cat["already_gave_neutered_message"] if "already_gave_neutered_message" in cat else False
+            new_cat.give_kittypet_message = cat["give_kittypet_message"] if "give_kittypet_message" in cat else False
             new_cat.vaccinated = cat["vaccinated"] if "vaccinated" in cat else False
             new_cat.exiled = cat["exiled"]
             new_cat.driven_out = cat["driven_out"] if "driven_out" in cat else False
